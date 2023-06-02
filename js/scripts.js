5bb383120e00397664c7ef1356940da7506b2b75
Jason E. Church <elijahchurch@gmail.com>
2023-06-02 12:00:39 -0700

    WIP: set variables correctly in JavaScript

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+window.onload = function () {
+
+    const form = document.querySelector("form#survey");
+
+    form.onsubmit = function (event) {
+        event.preventDefault
+
+        const C = document.getElementById("C#");
+        const Ruby = document.getElementById("Ruby");
+        const Rust = document.getElementById("Rust");
+        const Swift = document.getElementById("Swift");
+
+        let answer1 = document.getElementById("answer1").value;
+    };
+
+
+
+
+};
