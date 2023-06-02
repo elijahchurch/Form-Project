f89f2bfe9d3984f06c73a6ef425d4156e0d973ca
Jason E. Church <elijahchurch@gmail.com>
2023-06-02 14:38:36 -0700

    fix javascript logic so all results generate

--- js/scripts.js
+++ js/scripts.js
@@ -13,16 +13,21 @@ window.onload = function () {
         rustResult.setAttribute("class", "hidden")
         const swiftResult = document.getElementById("Swift");
         swiftResult.setAttribute("class", "hidden");
+        const youngResult = document.getElementById("young");
+        youngResult.setAttribute("class", "hidden");
 
         const age = parseInt(document.getElementById("answer1").value);
         const pets = parseInt(document.getElementById("answer2").value);
 
         if (age >= "20" && pets >= "2") {
             cResult.removeAttribute("class");
-        } if else (age >= "20" && pets <= "1") {
+        } else if (age >= "20" && pets === "1") {
             rubyResult.removeAttribute("class");
-        } if else (age >= "8" && age < "20") {
+        } else if (age > "8" && age < "20") {
             rustResult.removeAttribute("class");
+        }
+        else if (age <= "8") {
+            youngResult.removeAttribute("class");
         } else {
             swiftResult.removeAttribute("class");
         }
